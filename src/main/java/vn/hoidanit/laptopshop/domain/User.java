cdb21f481ac571b89a949912d6dfbe3c7169bd2e
FongFox <phong.tgn.coder@gmail.com>
2024-04-29 18:48:40 +0700

    Video 58; Create table user in database laptop_shop (by using spring jpa)

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,7 +1,13 @@
 package vn.hoidanit.laptopshop.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String email;
     private String password;
     private String fullName;

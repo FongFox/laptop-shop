0399e5b7de46782c0bc8931a1b5ba54ec2bb0748
FongFox <phong.tgn.coder@gmail.com>
2024-05-08 23:00:43 +0700

    Video 84; Seperate admin layout to re-use; Init and add code for Order, Product controller; Update code for Dashboard controller, Order and Product Page.

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/OrderController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/OrderController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DashboardController {
-    @GetMapping("/admin")
-    public String getDashboardPage() {
-        return "admin/dashboard/show";
+public class OrderController {
+    @GetMapping("/admin/order")
+    public String getOrderPage() {
+        return "admin/order/show";
     }
 }
--- src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DashboardController {
-    @GetMapping("/admin")
-    public String getDashboardPage() {
-        return "admin/dashboard/show";
+public class ProductController {
+    @GetMapping("/admin/product")
+    public String getProductPage() {
+        return "admin/product/show";
     }
 }
